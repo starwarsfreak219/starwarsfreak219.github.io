b8503754971e2f9007687cacbdf3a11a20d6243e
starwarsfreak219 <clonecommandercody34@yahoo.com>
2022-10-03 15:43:44 -0400

    Working on animations for that coming soon text

---
+++ src/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+
+export default function ComingSoon() {
+  return(
+    <>
+      <div class='ComingSoon-bg'>
+        <h1>Coming Soon!</h1>
+      </div>
+    </>
+  );
+}
